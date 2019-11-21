1ff22083be0ccd3f7748e73327323a495a5124e3
Bobach22 <l.user2200@gmail.com>
2019-11-22 00:30:54 +0500

    add first GET route

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express=require('express')
 const app=express()
 
+const mockUserData=[
+    {name:'Mark'},{
+        name:'Jill'
+    }
+]
+app.get('/users',(req,res)=>{
+    res.json({
+        success:true,
+        message:'success',
+        users:mockUserData
+    })
+})
+
 app.listen(8000,()=>{console.log('Server is running on Port 8000')})
